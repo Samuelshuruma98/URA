0c3b9a90e80ff9117e39f0b6a3e9acb167aac296
Samuelshuruma98 <91179661+Samuelshuruma98@users.noreply.github.com>
2023-11-27 19:09:05 +0300

    Update models.js

--- models.js
+++ models.js
@@ -1,1 +1,19 @@
 //For models
+
+// models/payer.js
+const mongoose = require('mongoose');
+
+const payerSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  dob: { type: Date, required: true },
+  occupation: { type: String, required: true },
+  gender: { type: String, enum: ['male', 'female'], required: true },
+  phone: { type: String, required: true },
+  email: { type: String, required: true },
+  annualIncome: { type: Number, required: true },
+  tin: { type: String, required: true },
+});
+
+const Payer = mongoose.model('Payer', payerSchema);
+
+module.exports = Payer;
